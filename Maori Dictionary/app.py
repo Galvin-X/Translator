742c7b55f8cc7946f8713d9e47cf0a8fffb73c28
Galvin-X <18185@wc.school.nz>
2022-04-06 13:15:38 +1200

    Categories and words now displayed correctly Stylesheet changes Categories are displayed on the sidebar below the home page, rather than in the centre of the screen Each category uses the category.html file with its id passed in to display the data grabbed from the tables

--- Maori Dictionary/app.py
+++ Maori Dictionary/app.py
@@ -20,8 +20,7 @@ def create_connection(db_file):
     return None
 
 
-@app.route('/')
-def render_homepage():
+def find_categories():
     con = create_connection(DB_NAME)
 
     query = "SELECT id, name, description FROM category"
@@ -31,7 +30,50 @@ def render_homepage():
     found_categories = cur.fetchall()
     con.close()
 
-    return render_template('home.html', category_list=found_categories)
+    # print(found_categories)
+
+    return found_categories
+
+
+@app.route('/')
+def render_homepage():
+    # print(sqlite3.version)
+
+    return render_template('home.html', category_list=find_categories())
+
+
+@app.route('/category/<cat>')
+def render_category_page(cat):
+    con = create_connection(DB_NAME)
+
+    # Grab category data at the passed category
+    query = """SELECT id, name, description FROM category WHERE id = ?"""
+    cur = con.cursor()
+    cur.execute(query, (cat,))
+    category_data = cur.fetchall()
+
+    # print(category_data)
+
+    # Find words that meet the requirements (word ids in the word_tag table with the category_id of <cat>
+    query = """SELECT word_id FROM word_tag WHERE category_id = ?"""
+    cur = con.cursor()
+    cur.execute(query, (cat,))
+    found_words = cur.fetchall()
+
+    words_data = []
+
+    # Access word table by referencing the found words from the word_tag table, append into new list
+    for w in found_words:
+        cur = con.cursor()
+        cur.execute("""SELECT id, name, description, author, timestamp FROM word WHERE id = ?""", w)
+        words_data.append(cur.fetchall())
+
+    # Print words for debug + close connection to database
+    print(words_data)
+    con.close()
+
+    return render_template('category.html', word_list=words_data, passed_cat=category_data,
+                           category_list=find_categories(), )
 
 
 app.run(host='0.0.0.0', debug=True)
