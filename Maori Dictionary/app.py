ca5f6a0dfc825211d1238700be9803e022330982
Galvin-X <18185@wc.school.nz>
2022-05-10 10:12:14 +1200

    Teacher functionality Adding Categories Deleting words and categories

--- Maori Dictionary/app.py
+++ Maori Dictionary/app.py
@@ -68,22 +68,31 @@ def is_logged_in():
         return True
 
 
+def is_teacher():
+    if not is_logged_in() or (session.get('teacher') is None or 0):
+        print('Not teacher')
+        return False
+    else:
+        print('Is teacher')
+        return True
+
+
 @app.route('/')
 def render_homepage():
     return render_template('home.html', category_list=find_categories(), logged_in=is_logged_in(),
-                           user_account=my_account())
+                           user_account=my_account(), teacher=is_teacher())
 
 
 @app.route('/about')
 def render_about_page():
     return render_template('about.html', category_list=find_categories(), logged_in=is_logged_in(),
-                           user_account=my_account())
+                           user_account=my_account(), teacher=is_teacher())
 
 
 @app.route('/contributions')
 def render_contributions_page():
     return render_template('contributions.html', category_list=find_categories(), logged_in=is_logged_in(),
-                           user_account=my_account())
+                           user_account=my_account(), teacher=is_teacher())
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -95,7 +104,7 @@ def render_login_page():
         email = request.form['email'].strip().lower()
         password = request.form['pass1'].strip()
 
-        query = """SELECT id, username, password FROM user WHERE email = ?"""
+        query = """SELECT id, username, password, teacher FROM user WHERE email = ?"""
         con = create_connection(DB_NAME)
         cur = con.cursor()
         cur.execute(query, (email,))
@@ -106,6 +115,7 @@ def render_login_page():
             userid = user_data[0][0]
             username = user_data[0][1]
             db_password = user_data[0][2]
+            teacher_status = user_data[0][3]
         except IndexError:
             return redirect('/login?error=Email+or+password+incorrect')
 
@@ -120,11 +130,12 @@ def render_login_page():
         session['email'] = email
         session['userid'] = userid
         session['username'] = username
+        session['teacher'] = teacher_status
         print(session)
         return redirect('/')
 
     return render_template('login.html', category_list=find_categories(), logged_in=is_logged_in(),
-                           user_account=my_account())
+                           user_account=my_account(), teacher=is_teacher())
 
 
 @app.route('/account/<user>')
@@ -134,7 +145,7 @@ def account(user):
         return redirect('/?error=Account+unavailable')
 
     return render_template('account.html', category_list=find_categories(), logged_in=is_logged_in(),
-                           user_account=my_account(), user_data=current_user)
+                           user_account=my_account(), user_data=current_user, teacher=is_teacher())
 
 
 @app.route('/logout')
@@ -159,6 +170,7 @@ def render_signup_page():
         email = request.form.get('email').strip().lower()
         pass1 = request.form.get('pass1')
         pass2 = request.form.get('pass2')
+        teacher = request.form.get('teacher')
 
         # Check passwords
         if pass1 != pass2:
@@ -173,12 +185,12 @@ def render_signup_page():
         # Connect to DB
         con = create_connection(DB_NAME)
 
-        query = "INSERT INTO user(id, username, email, password) VALUES(NULL,?,?,?)"
+        query = "INSERT INTO user(id, username, email, password, teacher) VALUES(NULL,?,?,?,?)"
 
         # Execute query
         cur = con.cursor()
         try:
-            cur.execute(query, (username, email, hashed_pass))
+            cur.execute(query, (username, email, hashed_pass, teacher))
         except sqlite3.IntegrityError:
             return redirect('/signup?error=Email+is+already+used')
         con.commit()
@@ -187,7 +199,7 @@ def render_signup_page():
         return redirect('login')
 
     return render_template('signup.html', category_list=find_categories(), logged_in=is_logged_in(),
-                           user_account=my_account())
+                           user_account=my_account(), teacher=is_teacher())
 
 
 @app.route('/category/<cat>')
@@ -222,10 +234,10 @@ def render_category_page(cat):
 
     return render_template('category.html', word_list=words_data, passed_cat=category_data,
                            category_list=find_categories(), from_category=cat, logged_in=is_logged_in(),
-                           user_account=my_account())
+                           user_account=my_account(), teacher=is_teacher())
 
 
-@app.route('/word/<word>', methods=['GET', 'POST'])
+@app.route('/word/<word>')
 def render_word_page(word):
     con = create_connection(DB_NAME)
 
@@ -237,14 +249,75 @@ def render_word_page(word):
 
     con.close()
 
-    return render_template('word.html', passed_word=word_data,
-                           category_list=find_categories(), logged_in=is_logged_in(), user_account=my_account())
+    return render_template('word.html', passed_word=word_data, category_list=find_categories(),
+                           logged_in=is_logged_in(), user_account=my_account(), teacher=is_teacher())
+
+
+@app.route('/remove_word/<word>')
+def render_word_remove_page(word):
+    if not is_logged_in():
+        return redirect('/?error=Not+logged+in')
+
+    if not is_teacher():
+        return redirect('/?error=No+permission')
+
+    con = create_connection(DB_NAME)
+
+    query = "DELETE FROM word WHERE id=?"
+    cur = con.cursor()
+
+    cur.execute(query, (word,))
+
+    con.commit()
+
+    query = "DELETE FROM word_tag WHERE word_id=?"
+    cur = con.cursor()
+
+    cur.execute(query, (word,))
+
+    con.commit()
+    con.close()
+
+    return redirect('/')
+
+
+@app.route('/remove_category/<cat>')
+def render_category_remove_page(cat):
+    if not is_logged_in():
+        return redirect('/?error=Not+logged+in')
+
+    if not is_teacher():
+        return redirect('/?error=No+permission')
+
+    con = create_connection(DB_NAME)
+
+    query = "SELECT user_created FROM category WHERE id=?"
+    cur = con.cursor()
+
+    cur.execute(query, (cat, ))
+    created_by_user = cur.fetchall()
+
+    if created_by_user[0][0]:
+        query = "DELETE FROM category WHERE id=?"
+        cur = con.cursor()
+
+        cur.execute(query, (cat,))
+
+        con.commit()
+        con.close()
+    else:
+        return redirect('/?error=No+permission')
+
+    return redirect('/')
 
 
 @app.route('/add_word', methods=['GET', 'POST'])
 def render_add_word_page():
     if not is_logged_in():
         return redirect('/?error=Not+logged+in')
+
+    if not is_teacher():
+        return redirect('/?error=No+permission')
 
     if request.method == "POST":
         word_name = request.form['word_name'].strip()
@@ -269,7 +342,6 @@ def render_add_word_page():
         con.commit()
 
         word_data = cur.fetchall()
-        w_id = len(word_data) + 1
 
         for find_word in word_data:
             if find_word[0].strip().lower() == word_name.strip().lower():
@@ -283,11 +355,20 @@ def render_add_word_page():
 
         con.commit()
 
+        query = "SELECT id FROM word ORDER BY id DESC"
+        cur = con.cursor()
+        cur.execute(query)
+
+        list_of_ids = cur.fetchall()
+        print(list_of_ids)
+
+        latest_id = list_of_ids[0][0]
+
         # Add word to word tag
         query = """INSERT INTO word_tag(word_id, category_id) VALUES (?,?)"""
         cur = con.cursor()
 
-        cur.execute(query, (w_id, cat_id))
+        cur.execute(query, (latest_id, cat_id))
 
         con.commit()
         con.close()
@@ -295,7 +376,53 @@ def render_add_word_page():
         return redirect('/')
 
     return render_template('add_word.html', category_list=find_categories(), logged_in=is_logged_in(),
-                           user_account=my_account())
+                           user_account=my_account(), teacher=is_teacher())
+
+
+@app.route('/add_category', methods=['GET', 'POST'])
+def render_add_category_page():
+    if not is_logged_in():
+        return redirect('/?error=Not+logged+in')
+
+    if not is_teacher():
+        return redirect('/?error=No+permission')
+
+    if request.method == "POST":
+        cat_name = request.form['word_name'].strip()
+        cat_desc = request.form['word_desc'].strip()
+
+        userid = session['userid']
+        timestamp = datetime.now()
+
+        con = create_connection(DB_NAME)
+
+        # FIND IF WORD ALREADY EXISTS
+        query = """SELECT name FROM category"""
+        cur = con.cursor()
+        cur.execute(query)
+
+        con.commit()
+
+        cat_data = cur.fetchall()
+        c_id = len(cat_data) + 1
+
+        for find_cat in cat_data:
+            if find_cat[0].strip().lower() == cat_name.strip().lower():
+                return redirect('/?error=Category+already+exists+in+database')
+
+        query = "INSERT INTO category(id,name,description,timestamp,user_created) VALUES (NULL,?,?,?,?)"
+        cur = con.cursor()
+
+        # Catch insertion errors
+        cur.execute(query, (cat_name, cat_desc, timestamp, 1))
+
+        con.commit()
+        con.close()
+
+        return redirect('/')
+
+    return render_template('add_category.html', category_list=find_categories(), logged_in=is_logged_in(),
+                           user_account=my_account(), teacher=is_teacher())
 
 
 app.run(host='0.0.0.0', debug=True)
